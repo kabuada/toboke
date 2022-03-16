6132bad294b3996a8d1786c53c4a89bda5330def
kabuada <101716506+kabuada@users.noreply.github.com>
2022-03-16 14:08:06 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # configs
-AUUID=UUID
+AUUID=ea8f10e5-8f04-429e-8d95-54c3d0388d8f
 CADDYIndexPage=https://github.com/PavelDoGreat/WebGL-Fluid-Simulation/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/账号名/仓库名/master/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/账号名/仓库名/master/etc/xray.json
+CONFIGCADDY=https://raw.githubusercontent.com/kabuada/toboke/master/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/kabuada/toboke/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/账号名/仓库名/master/etc/StoreFiles
+StoreFiles=https://raw.githubusercontent.com/kabuada/toboke/master/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
